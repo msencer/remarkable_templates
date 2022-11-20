99b3d5f5772627f887dd29753932aea105831e5f
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2022-11-20 17:11:06 +0100

    Remove the possible duplicates from templates.json caused by multiple installations

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ scp templates/pngs/*.png rem:/usr/share/remarkable/templates
 # Receiving the current templates.json to add our custom templates
 scp rem:/usr/share/remarkable/templates/templates.json ./templates.json
 # Adding the custom templates to templates.json
-jq -n '{ templates: [ inputs.templates ] | add }' templates.json templates.addition.json > templates.merged.json
+jq -n '{ templates: [ inputs.templates ] | add | unique_by(.name)}' templates.json templates.addition.json > templates.merged.json
 # Taking a backup of the templates.json in any case
 ssh rem "cp /usr/share/remarkable/templates/templates.json /usr/share/remarkable/templates/templates.backup.json"
 # Replacing the templates.json on the device with merged one
