326e51e0b7b5167fb4860ad133d0f2247320e672
Geert Jan van Ouwendorp <geertjan@GJs-MacBoekie.local>
2023-03-01 20:31:15 +0100

    Make install script ssh as root (not current host user)

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 # Sending the PNG files to reMarkable
-scp templates/pngs/*.png rem:/usr/share/remarkable/templates
+scp templates/pngs/*.png root@rem:/usr/share/remarkable/templates
+
 # Receiving the current templates.json to add our custom templates
-scp rem:/usr/share/remarkable/templates/templates.json ./templates.json
+scp root@rem:/usr/share/remarkable/templates/templates.json ./templates.json
+
 # Adding the custom templates to templates.json
 jq -n '{ templates: [ inputs.templates ] | add | unique_by(.name)}' templates.json templates.addition.json > templates.merged.json
+
 # Taking a backup of the templates.json in any case
-ssh rem "cp /usr/share/remarkable/templates/templates.json /usr/share/remarkable/templates/templates.backup.json"
+ssh root@rem "cp /usr/share/remarkable/templates/templates.json /usr/share/remarkable/templates/templates.backup.json"
+
 # Replacing the templates.json on the device with merged one
-scp templates.merged.json rem:/usr/share/remarkable/templates/templates.json
+scp templates.merged.json root@rem:/usr/share/remarkable/templates/templates.json
+
 # Restarting the GUI service
-ssh rem "systemctl restart xochitl"
+ssh root@rem "systemctl restart xochitl"
 
